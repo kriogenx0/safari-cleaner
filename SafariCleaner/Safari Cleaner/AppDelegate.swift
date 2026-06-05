6d9fd9c8db0612cb1d9067aac97a3352c203a8d0
Alex Vaos <simplex0@gmail.com>
2026-06-04 18:28:15 -0700

    macOS window fix

--- SafariCleaner/Safari Cleaner/AppDelegate.swift
+++ SafariCleaner/Safari Cleaner/AppDelegate.swift
@@ -1,13 +1,14 @@
 import Cocoa
+import SwiftUI
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     private var window: NSWindow?
 
     func applicationDidFinishLaunching(_ n: Notification) {
-        let vc = ViewController()
+        let vc = NSHostingController(rootView: BookmarkReviewView())
         let win = NSWindow(
-            contentRect: NSRect(x: 196, y: 200, width: 480, height: 500),
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 500),
             styleMask: [.titled, .closable, .miniaturizable, .resizable],
             backing: .buffered,
             defer: false
@@ -15,7 +16,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         win.minSize = NSSize(width: 380, height: 400)
         win.title = "Safari Cleaner"
         win.contentViewController = vc
+        win.center()
         win.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
         window = win
     }
 
--- SafariCleaner/Safari Cleaner/ViewController.swift
+++ SafariCleaner/Safari Cleaner/ViewController.swift
@@ -341,24 +341,3 @@ struct BookmarkReviewView: View {
         }
     }
 }
-
-// MARK: - View Controller
-
-class ViewController: NSViewController {
-    override func loadView() {
-        view = NSView(frame: NSRect(x: 0, y: 0, width: 480, height: 500))
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let host = NSHostingView(rootView: BookmarkReviewView())
-        host.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(host)
-        NSLayoutConstraint.activate([
-            host.topAnchor.constraint(equalTo: view.topAnchor),
-            host.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            host.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            host.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ])
-    }
-}
